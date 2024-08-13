5fe32883ed4a04dd7e528a6a29685a26eb3860dd
Ricardo Santos <twidxuga@gmail.com>
2024-08-13 15:13:39 +0100

    Tue 13 Aug 15:13:39 BST 2024 - Scripted dotfile sync from twidarch

---
+++ wezterm/dot-config/wezterm/wezterm.lua
@@ -0,0 +1,21 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+config.color_scheme = 'nord'
+config.font = wezterm.font 'DejaVuSansM Nerd Font Mono'
+-- config.font = wezterm.font 'CommitMono Nerd Font Mono'
+-- config.font = wezterm.font 'CodeNewRoman Nerd Font Mono'
+config.font_size = 10.4
+config.enable_tab_bar = false
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+
+-- and finally, return the configuration to wezterm
+return config
