0d1446c1cbe9f40167c969597459edfece05e331
twidxuga@gmail.com <twidxuga@gmail.com>
2024-10-01 13:17:55 +0100

    Tue  1 Oct 13:17:55 BST 2024 - Scripted dotfile sync from twidarch

---
+++ nvim/dot-config/nvim/lua/plugins/codeium.lua
@@ -0,0 +1,14 @@
+return {
+    "Exafunction/codeium.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "hrsh7th/nvim-cmp",
+    },
+    opts = {
+      enable_chat = true,
+    },
+    -- config = function()
+    --     require("codeium").setup({
+    --     })
+    -- end
+}
