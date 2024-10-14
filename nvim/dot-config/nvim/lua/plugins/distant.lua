aa26416559a13f15438ab4345b50238aa5ec82a6
twidxuga@gmail.com <twidxuga@gmail.com>
2024-10-14 20:56:37 +0100

    Mon 14 Oct 20:56:37 BST 2024 - Scripted dotfile sync from twidarch

--- nvim/dot-config/nvim/lua/plugins/distant.lua
+++
@@ -1,8 +0,0 @@
-return {
-    -- Remote file editing via ssh simplified
-    'chipsenkbeil/distant.nvim', 
-    branch = 'v0.3',
-    config = function()
-        require('distant'):setup()
-    end
-}
