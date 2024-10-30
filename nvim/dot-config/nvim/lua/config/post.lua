72f478c8f20ca1eb61465f991be92d1bd5a8463e
twidxuga@gmail.com <twidxuga@gmail.com>
2024-10-30 18:20:14 +0000

    Wed 30 Oct 18:20:14 GMT 2024 - Scripted dotfile sync from twidarch

--- nvim/dot-config/nvim/lua/config/post.lua
+++ nvim/dot-config/nvim/lua/config/post.lua
@@ -1,11 +1,11 @@
 -- Post config - Custom config after lazy and plugins are initialized
 
--- Vertical help
-vim.api.nvim_create_user_command("H", "vertical bo help <args>", { nargs = '?', desc = "Vertical help" })
-vim.api.nvim_create_user_command("Help", "vertical bo help <args>", { nargs = '?', desc = "Vertical help" })
-
 -- Create a custom highlight for Flash, overriding the default
 vim.api.nvim_set_hl(0, "FlashLabel", { foreground = "#ffffff", background = "#565d6c" })
+
+-- Vertical help
+vim.api.nvim_create_user_command("H", "vertical bo help <args>", { nargs = "?", desc = "Vertical help" })
+vim.api.nvim_create_user_command("Help", "vertical bo help <args>", { nargs = "?", desc = "Vertical help" })
 
 -- Sesssion commands
 vim.api.nvim_create_user_command(
@@ -13,5 +13,13 @@ vim.api.nvim_create_user_command(
   ":source ~/Documents/QuickAccess/Session.vim",
   { desc = "Start Twid's session" }
 )
-vim.api.nvim_create_user_command("Monadd", ":source ~/Monadd/Session.vim", { desc = "Start Monadd's session" })
-
+vim.api.nvim_create_user_command(
+  "CV",
+  ":source ~/Documents/CV/Session.vim",
+  { desc = "Start CV editing session" }
+)
+vim.api.nvim_create_user_command(
+  "Monadd",
+  ":source ~/Monadd/Session.vim",
+  { desc = "Start Monadd's session" }
+)
