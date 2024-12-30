4e17611a5e271034dccba34c56bb492ba282c70d
twidxuga@gmail.com <twidxuga@gmail.com>
2024-12-30 19:15:47 +0000

    Mon 30 Dec 19:15:47 GMT 2024 - Scripted dotfile sync from twidarch

--- nvim/dot-config/nvim/lua/plugins/lsp.lua
+++ nvim/dot-config/nvim/lua/plugins/lsp.lua
@@ -13,6 +13,17 @@ return {
       },
     },
   },
+  -- formatters, etc
+  -- {
+  --   "williamboman/mason.nvim",
+  --   opts = {
+  --     ensure_installed = {
+  --       -- "xmlformatter",
+  --       "eslint_d"
+  --     },
+  --   },
+  -- },
+  -- -- Language servers
   -- {
   --   "williamboman/mason-lspconfig.nvim",
   --   opts = {
@@ -20,16 +31,50 @@ return {
   --     ensure_installed = {},
   --   }
   -- },
-  { -- lsp virtualenv select
-    'linux-cultist/venv-selector.nvim',
-    dependencies = { 'neovim/nvim-lspconfig', 'nvim-telescope/telescope.nvim', 'mfussenegger/nvim-dap-python' },
+  -- {
+  --   "mfussenegger/nvim-lint",
+  --   event = {
+  --     "BufReadPre",
+  --     "BufNewFile",
+  --   },
+  --   config = function()
+  --     local lint = require("lint")
+  --
+  --     lint.linters_by_ft = {
+  --       javascript = { "eslint_d" },
+  --       typescript = { "eslint_d" },
+  --       javascriptreact = { "eslint_d" },
+  --       typescriptreact = { "eslint_d" },
+  --       -- svelte = { "eslint_d" },
+  --       -- kotlin = { "ktlint" },
+  --       -- terraform = { "tflint" },
+  --     }
+  --
+  --     local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+  --
+  --     vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+  --       group = lint_augroup,
+  --       callback = function()
+  --         lint.try_lint()
+  --       end,
+  --     })
+  --
+  --     -- vim.keymap.set("n", "<leader>ll", function()
+  --     --   lint.try_lint()
+  --     -- end, { desc = "Trigger linting for current file" })
+  --   end,
+  -- },
+  -- -- lsp virtualenv select
+  {
+    "linux-cultist/venv-selector.nvim",
+    dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
     opts = {
       -- Your options go here
       -- name = "venv",
       -- auto_refresh = false
-      dap_enabled = true -- requires debugpy
+      dap_enabled = true, -- requires debugpy
     },
-    event = 'VeryLazy', -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
+    event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
     -- keys = {
     --   -- Keymap to open VenvSelector to pick a venv.
     --   { '<leader>vs', '<cmd>VenvSelect<cr>' },
