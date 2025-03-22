f7d13eb8bc102a69a5ddb9e9375533c9a145214b
twidxuga@gmail.com <twidxuga@gmail.com>
2025-03-22 19:46:27 +0000

    make nvidia default, optimus not working

--- screenlayout/dot-screenlayout/hdmi.sh
+++ screenlayout/dot-screenlayout/hdmi.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-xrandr --output eDP-1 --off --output DP-2 --off --output DP-3 --off --output HDMI-1-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1-0 --off --output DP-1-1 --off
+# xrandr --output eDP-1 --off --output DP-2 --off --output DP-3 --off --output HDMI-1-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1-0 --off --output DP-1-1 --off
+xrandr --output HDMI-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-0 --off --output DP-1 --off --output eDP-1-1 --off --output DP-1-2 --off --output DP-1-3 --off
