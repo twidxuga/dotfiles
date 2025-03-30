e9b20c8e6ec91b0c2cdc53de7f7143faeecad039
twidxuga@gmail.com <twidxuga@gmail.com>
2025-03-30 15:56:06 +0100

    update markdown rendering

--- nvim/dot-config/nvim/lua/plugins/ai.lua
+++ nvim/dot-config/nvim/lua/plugins/ai.lua
@@ -118,14 +118,14 @@ return {
           },
         },
       },
-      {
-        -- Make sure to set this up properly if you have lazy=true
-        'MeanderingProgrammer/render-markdown.nvim',
-        opts = {
-          file_types = { "markdown", "Avante" },
-        },
-        ft = { "markdown", "Avante" },
-      },
+      -- { -- commented on replacement by markview.nvim
+      --   -- Make sure to set this up properly if you have lazy=true
+      --   'MeanderingProgrammer/render-markdown.nvim',
+      --   opts = {
+      --     file_types = { "markdown", "Avante" },
+      --   },
+      --   ft = { "markdown", "Avante" },
+      -- },
     },
   },
   -- {
