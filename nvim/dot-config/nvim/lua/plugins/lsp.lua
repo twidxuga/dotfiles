9c2a00378967bf1b905b42fb683979ac452e3ae1
twidxuga@gmail.com <twidxuga@gmail.com>
2025-06-12 10:20:43 +0100

    back on razer

--- nvim/dot-config/nvim/lua/plugins/lsp.lua
+++ nvim/dot-config/nvim/lua/plugins/lsp.lua
@@ -26,6 +26,11 @@ return {
       },
     },
   },
+  -- Mason workaround
+  --
+  -- { "mason-org/mason.nvim", version = "^1.0.0" },
+  -- { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
+  -- https://github.com/LazyVim/LazyVim/issues/6039
   {
     "williamboman/mason.nvim",
     opts = {
