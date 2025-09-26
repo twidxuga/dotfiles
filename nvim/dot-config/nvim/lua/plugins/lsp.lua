165ec951405c4ac389ee2ca775032f12d089da76
twidxuga <twidxuga@gmail.com>
2025-09-26 18:57:45 +0100

    env upgrade

--- nvim/dot-config/nvim/lua/plugins/lsp.lua
+++ nvim/dot-config/nvim/lua/plugins/lsp.lua
@@ -39,6 +39,10 @@ return {
         "js-debug-adapter",
         "prettier",
       },
+      registries = {
+          "github:mason-org/mason-registry", -- main mason
+          "github:Crashdummyy/mason-registry", -- required for rosly
+      },
     }
   },
   {
@@ -136,20 +140,20 @@ return {
   --             package = "",
   --           },
   --         mappings = {
-  --           run_test_from_buffer = { lhs = "<leader>r", desc = "run test from buffer" },
-  --           peek_stack_trace_from_buffer = { lhs = "<leader>p", desc = "peek stack trace from buffer" },
-  --           filter_failed_tests = { lhs = "<leader>fe", desc = "filter failed tests" },
-  --           debug_test = { lhs = "<leader>d", desc = "debug test" },
-  --           go_to_file = { lhs = "g", desc = "go to file" },
-  --           run_all = { lhs = "<leader>R", desc = "run all tests" },
-  --           run = { lhs = "<leader>r", desc = "run test" },
-  --           peek_stacktrace = { lhs = "<leader>p", desc = "peek stacktrace of failed test" },
-  --           expand = { lhs = "o", desc = "expand" },
-  --           expand_node = { lhs = "E", desc = "expand node" },
-  --           expand_all = { lhs = "-", desc = "expand all" },
-  --           collapse_all = { lhs = "W", desc = "collapse all" },
-  --           close = { lhs = "q", desc = "close testrunner" },
-  --           refresh_testrunner = { lhs = "<C-r>", desc = "refresh testrunner" }
+  --           -- run_test_from_buffer = { lhs = "<leader>r", desc = "run test from buffer" },
+  --           -- peek_stack_trace_from_buffer = { lhs = "<leader>p", desc = "peek stack trace from buffer" },
+  --           -- filter_failed_tests = { lhs = "<leader>fe", desc = "filter failed tests" },
+  --           -- debug_test = { lhs = "<leader>d", desc = "debug test" },
+  --           -- go_to_file = { lhs = "g", desc = "go to file" },
+  --           -- run_all = { lhs = "<leader>R", desc = "run all tests" },
+  --           -- run = { lhs = "<leader>r", desc = "run test" },
+  --           -- peek_stacktrace = { lhs = "<leader>p", desc = "peek stacktrace of failed test" },
+  --           -- expand = { lhs = "o", desc = "expand" },
+  --           -- expand_node = { lhs = "E", desc = "expand node" },
+  --           -- expand_all = { lhs = "-", desc = "expand all" },
+  --           -- collapse_all = { lhs = "W", desc = "collapse all" },
+  --           -- close = { lhs = "q", desc = "close testrunner" },
+  --           -- refresh_testrunner = { lhs = "<C-r>", desc = "refresh testrunner" }
   --         },
   --         --- Optional table of extra args e.g "--blame crash"
   --         additional_args = {}
@@ -232,4 +236,13 @@ return {
   --     end)
   --   end
   -- }
+  {
+    "seblyng/roslyn.nvim",
+    ---@module 'roslyn.config'
+    ---@type RoslynNvimConfig
+    opts = {
+      broad_search = true -- default is false
+        -- your configuration comes here; leave empty for default settings
+    },
+  }
 }
